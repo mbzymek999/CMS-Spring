51a9f32f379726a96af38658bf424f35636564ec
Bzymek Mateusz <mbzymek99@wp.pl>
2021-11-12 00:14:21 +0100

    Transfer of functionality to create a agreement from the controller to the service

--- src/main/java/cms/api/company/controller/AgreementController.java
+++ src/main/java/cms/api/company/controller/AgreementController.java
@@ -1,135 +1,32 @@
 package cms.api.company.controller;
 
 import cms.api.company.request.AgreementRequest;
-import cms.config.security.payload.response.MessageResponse;
-import cms.config.security.services.UserDetailsImpl;
-import cms.domain.company.entity.Agreement;
-import cms.domain.company.entity.Company;
-import cms.domain.company.repository.AgreementRepository;
-import cms.domain.company.repository.CompanyRepository;
-import cms.domain.employee.entity.Employee;
-import cms.domain.employee.repository.EmployeeRepository;
-import cms.domain.user.entity.ERole;
-import cms.domain.user.entity.Role;
-import cms.domain.user.entity.User;
-import cms.domain.user.repository.RoleRepository;
-import cms.domain.user.repository.UserRepository;
+import cms.domain.company.service.AgreementService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.HashSet;
-import java.util.Set;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/auth")
 public class AgreementController {
-
-    private final UserRepository userRepository;
-
-    private final CompanyRepository companyRepository;
-
-    private final EmployeeRepository employeeRepository;
-
-    private final RoleRepository roleRepository;
-
-    private final PasswordEncoder encoder;
-
-    private final AgreementRepository agreementRepository;
-
-    public AgreementController(UserRepository userRepository, CompanyRepository companyRepository, EmployeeRepository employeeRepository, RoleRepository roleRepository, PasswordEncoder encoder, AgreementRepository agreementRepository) {
-        this.userRepository = userRepository;
-        this.companyRepository = companyRepository;
-        this.employeeRepository = employeeRepository;
-        this.roleRepository = roleRepository;
-        this.encoder = encoder;
-        this.agreementRepository = agreementRepository;
+    private final AgreementService service;
+    Logger logger = LoggerFactory.getLogger(AgreementController.class);
+    public AgreementController(AgreementService service) {
+        this.service = service;
     }
 
-    @PostMapping("/signup/employee")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody AgreementRequest signUpRequest) {
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Nazwa użytkownika zajęta"));
+    @PostMapping("/create/agreement")
+    @PreAuthorize("hasRole('COMPANY')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> addNewAgreement(@RequestBody AgreementRequest request) {
+        try {
+            return ResponseEntity.ok(service.registerUser(request));
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
         }
-
-        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Email jest w zajęty"));
-        }
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetailsImpl userImpl = (UserDetailsImpl)authentication.getPrincipal();
-
-        Company company = companyRepository.findById(userImpl.getId()).orElse(null);
-        if(company == null){
-            System.out.println("Firma nie istnieje");
-        }
-
-        // Create new user's account
-        User user = new User(signUpRequest.getUsername(),
-                signUpRequest.getEmail(),
-                encoder.encode(signUpRequest.getPassword()));
-
-        // Create new Employee
-        Employee employee = new Employee(
-                signUpRequest.getName(),
-                signUpRequest.getLastName(),
-                signUpRequest.getPosition(),
-                signUpRequest.getPhone(),
-                signUpRequest.getStreet(),
-                signUpRequest.getStreetNumber(),
-                signUpRequest.getBuildingNumber(),
-                signUpRequest.getCity(),
-                signUpRequest.getPostcode(),
-                user
-        );
-
-        // Create new agreement
-        Agreement agreement = new Agreement(
-                signUpRequest.getAgreementType(),
-                signUpRequest.getAssignedDate(),
-                signUpRequest.getDateFrom(),
-                signUpRequest.getDateTo(),
-                signUpRequest.getSalary(),
-                company,
-                user,
-                employee
-        );
-
-
-        Set<String> strRoles = signUpRequest.getRole();
-        Set<Role> roles = new HashSet<>();
-
-        if (strRoles == null) {
-            Role employeeRole = roleRepository.findByName(ERole.ROLE_EMPLOYEE)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(employeeRole);
-        } else {
-            strRoles.forEach(role -> {
-                if ("employee".equals(role)) {
-                    Role employeeRole = roleRepository.findByName(ERole.ROLE_EMPLOYEE)
-                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                    roles.add(employeeRole);
-                } else {
-                    Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                    roles.add(userRole);
-                }
-            });
-        }
-
-        user.setRoles(roles);
-        userRepository.save(user);
-        employeeRepository.save(employee);
-        agreementRepository.save(agreement);
-
-        return ResponseEntity.ok(new MessageResponse("Employee registered successfully!"));
     }
 }
---
+++ src/main/java/cms/domain/company/service/AgreementService.java
@@ -0,0 +1,125 @@
+package cms.domain.company.service;
+
+import cms.api.company.request.AgreementRequest;
+import cms.config.security.services.UserDetailsImpl;
+import cms.domain.company.entity.Agreement;
+import cms.domain.company.entity.Company;
+import cms.domain.company.repository.AgreementRepository;
+import cms.domain.company.repository.CompanyRepository;
+import cms.domain.employee.entity.Employee;
+import cms.domain.employee.repository.EmployeeRepository;
+import cms.domain.user.entity.ERole;
+import cms.domain.user.entity.Role;
+import cms.domain.user.entity.User;
+import cms.domain.user.repository.RoleRepository;
+import cms.domain.user.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class AgreementService {
+
+    private final UserRepository userRepository;
+
+    private final CompanyRepository companyRepository;
+
+    private final EmployeeRepository employeeRepository;
+
+    private final RoleRepository roleRepository;
+
+    private final PasswordEncoder encoder;
+
+    private final AgreementRepository agreementRepository;
+
+    public AgreementService(UserRepository userRepository, CompanyRepository companyRepository, EmployeeRepository employeeRepository, RoleRepository roleRepository, PasswordEncoder encoder, AgreementRepository agreementRepository) {
+        this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
+        this.employeeRepository = employeeRepository;
+        this.roleRepository = roleRepository;
+        this.encoder = encoder;
+        this.agreementRepository = agreementRepository;
+    }
+
+    public String registerUser(AgreementRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return"Error: Nazwa użytkownika zajęta";
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return "Error: Email jest w zajęty";
+        }
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userImpl = (UserDetailsImpl)authentication.getPrincipal();
+
+        Company company = companyRepository.findById(userImpl.getId()).orElse(null);
+        if(company == null){
+            System.out.println("Firma nie istnieje");
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        // Create new Employee
+        Employee employee = new Employee(
+                signUpRequest.getName(),
+                signUpRequest.getLastName(),
+                signUpRequest.getPosition(),
+                signUpRequest.getPhone(),
+                signUpRequest.getStreet(),
+                signUpRequest.getStreetNumber(),
+                signUpRequest.getBuildingNumber(),
+                signUpRequest.getCity(),
+                signUpRequest.getPostcode(),
+                user
+        );
+
+        // Create new agreement
+        Agreement agreement = new Agreement(
+                signUpRequest.getAgreementType(),
+                signUpRequest.getAssignedDate(),
+                signUpRequest.getDateFrom(),
+                signUpRequest.getDateTo(),
+                signUpRequest.getSalary(),
+                company,
+                user,
+                employee
+        );
+
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role employeeRole = roleRepository.findByName(ERole.ROLE_EMPLOYEE)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(employeeRole);
+        } else {
+            strRoles.forEach(role -> {
+                if ("employee".equals(role)) {
+                    Role employeeRole = roleRepository.findByName(ERole.ROLE_EMPLOYEE)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(employeeRole);
+                } else {
+                    Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+        employeeRepository.save(employee);
+        agreementRepository.save(agreement);
+
+        return "Employee registered successfully!";
+    }
+
+}
