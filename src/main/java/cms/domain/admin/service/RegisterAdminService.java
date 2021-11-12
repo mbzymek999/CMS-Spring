aa1117bb605ae80755cc3ed2d4fd4f9f5705b98a
Bzymek Mateusz <mbzymek99@wp.pl>
2021-11-12 01:17:04 +0100

    Transfer of functionality to create a admin from the controller to the service

---
+++ src/main/java/cms/domain/admin/service/RegisterAdminService.java
@@ -0,0 +1,69 @@
+package cms.domain.admin.service;
+
+import cms.api.admin.request.SignupAdminRequest;
+import cms.domain.user.entity.ERole;
+import cms.domain.user.entity.Role;
+import cms.domain.user.entity.User;
+import cms.domain.user.repository.RoleRepository;
+import cms.domain.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RegisterAdminService {
+
+    private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
+
+    private final PasswordEncoder encoder;
+
+    public RegisterAdminService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.encoder = encoder;
+    }
+
+    public String registerAdmin(SignupAdminRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return "Error: Nazwa użytkownika zajęta";
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return "Error: Email jest w zajęty";
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(adminRole);
+        } else {
+            strRoles.forEach(role -> {
+                if ("admin".equals(role)) {
+                    Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(adminRole);
+                } else {
+                    Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return "Admin registered successfully!";
+    }
+}
