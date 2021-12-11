3fbd19d07ad55027959d2bdd21ac594f0e848bf5
Bzymek Mateusz <mbzymek99@wp.pl>
2021-12-11 23:57:35 +0100

    Created currentPage field in the ReadCompanyTasksResponse.

--- src/main/java/cms/api/company/task/ReadCompanyTasksResponse.java
+++ src/main/java/cms/api/company/task/ReadCompanyTasksResponse.java
@@ -11,11 +11,13 @@ public class ReadCompanyTasksResponse {
 
     Long totalElements;
     int totalPages;
+    int currentPage;
     List<TaskCompanyReadModel> tasks;
 
     public ReadCompanyTasksResponse(Page<Task> page, List<TaskCompanyReadModel> tasks) {
         this.totalElements = page.getTotalElements();
         this.totalPages = page.getTotalPages();
+        this.currentPage = page.getNumber();
         this.tasks = tasks;
     }
 }
